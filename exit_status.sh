ce431c8529f4b18a1bad077bb0a7ff266a325d4b
Marco Alfaro <marcolcal@gmail.com>
2019-08-14 11:02:10 -0400

    Added new scripts to create users using positional parameters

---
+++ exit_status.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#ls -la ./
+#echo $?
+
+
+some=Same
+the=Also
+
+if [ $some = $the ]
+    then
+       echo $some "is the same as" $the
+    else
+       echo $some "is not the same as" $the
+fi
---
+++ user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo $USER
+
+if [ $USER = marcolcal ]
+   then
+      echo "Hello marco"
+   else
+      echo "You are not marco"
+fi
